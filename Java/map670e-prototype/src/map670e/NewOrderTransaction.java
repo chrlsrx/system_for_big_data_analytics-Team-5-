c3aa75ccd3072ccbe9e43f31588d663c7f51edf6
Babacar <babacar.sow@telecom-paristech.fr>
2020-10-28 14:49:55 +0100

    NewOrderTransaction Creation

---
+++ Java/map670e-prototype/src/map670e/NewOrderTransaction.java
@@ -0,0 +1,15 @@
+package map670e;
+
+public class NewOrderTransaction {
+private int w_id;
+private DataGeneration data;
+public NewOrderTransaction(int w_id)
+{
+	this.w_id = w_id;
+	this.data = new DataGeneration(w_id);
+}
+public void NewOrderTransactionRun()
+{
+	
+}
+}
